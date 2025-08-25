cc668924cfe7e99f90646c9fa77bb47db3098ebf
Johnwickdev <126758225+Johnwickdev@users.noreply.github.com>
2025-08-25 16:21:30 +0530

    Use Tomcat and WebFlux starter, simplify logging

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // API base URL now points to the AWS Elastic Beanstalk deployment
-  apiBaseUrl: 'https://autotradebot-env.eba-adnie9a5.eu-north-1.elasticbeanstalk.com/auth'
+  apiBaseUrl: 'https://backendforautobot-production.up.railway.app/auth'
 };
